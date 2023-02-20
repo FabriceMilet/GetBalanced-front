8b9f1d044ae0ed8de77d7ada4fb9e4a9c8ce2ebb
FabriceMilet <milet.fab@gmail.com>
2023-02-20 15:46:45 +0100

    création du composant SignUp et du slice UserSlice

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../src/app/store';
+import './style/reset.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
