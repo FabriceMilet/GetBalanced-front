f257b1c010c3e6222c60f635fd8f1395f46f24a3
gautierfalaise <gautierfalaise@live.fr>
2023-02-21 10:34:51 +0100

    Ajout de area2 de la page accueil

--- src/Components/Home/Area2/Area2.js
+++ src/Components/Home/Area2/Area2.js
@@ -1,11 +1,20 @@
 import './Area2.scss';
+import area1Logo from './logoArea1.svg'
 
 function Area2() {
 
     return (
-        <>
-        <h1>Area2</h1>
-        </>
+        <div className="area2_container">
+            <div className="top_container">
+                <h2>blablabla blablabla blablablabla... blabla</h2>
+            </div>
+            <div className="bottom_container">
+                <img className='logoArea1' src={area1Logo} alt="logo d'illustration" />
+                <p className='area1_paragraphe'>lorem jdgonfobn dog jtoigh og idog oird dgiog do jrdoi jdfog
+                    gdl gndro gjdo rdgogi gjdiog dijog doig jrdio gdl gdogdr ogdr
+                    vjiod vrdoi jdfio jgird</p>
+            </div>
+        </div>
     );
 }
 
