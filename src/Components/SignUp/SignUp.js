b5c68d14a51a0a48fe91740ff2358cef1512456f
FabriceMilet <milet.fab@gmail.com>
2023-02-27 11:21:16 +0100

    petit réglage css

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -7,7 +7,7 @@ import { setFormData } from '../../feature/users.slice';
 function SignUp() {
 const isLogged = useSelector((state) => state.user.isLogged);
 const formData = useSelector((state) => state.user.formData);
-const {id} = useSelector((state) => state.user.userConnected);
+// const {id} = useSelector((state) => state.user.userConnected);
 const dispatch = useDispatch();
 // on veut créer ici une nouvelle copie de l'objet formData avec la propriété 
 // correspondant à la variable name et sa valeur associée
