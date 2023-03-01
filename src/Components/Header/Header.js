37f76d62136113c4f8b1fe9c0e412b2493b12a23
FabriceMilet <milet.fab@gmail.com>
2023-03-01 13:48:31 +0100

    ajout du lien vers dashboard

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,46 +1,51 @@
-import './Header.scss';
-import logoGetBalanced from "./test.svg"
-import avatar from "./avatar.png"
-import { useSelector, useDispatch } from 'react-redux'
+import "./Header.scss";
+import logoGetBalanced from "./test.svg";
+import avatar from "./avatar.png";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { logoutUser } from "../../feature/users.slice";
 
-
 function Header() {
-
   const dispatch = useDispatch();
   const userConnected = useSelector((state) => state.user.userConnected);
   // On récupère les données sur l'utilisateur :
   const isLogged = useSelector((state) => state.user.isLogged);
 
-  console.log("user", userConnected)
+  console.log("user", userConnected);
   const handleLogout = () => {
-    dispatch(logoutUser())
-  }
+    dispatch(logoutUser());
+  };
 
   return (
-    <header className='header-container'>
+    <header className="header-container">
       <Link to="/">
-        <img className='logoGetBalanced' src={logoGetBalanced} alt="" />
+        <img className="logoGetBalanced" src={logoGetBalanced} alt="" />
       </Link>
       {/* vérifie si l'utilisateur est connecté, et affiche une nav ou une autre */}
-      {!isLogged ?
-        <nav className='header_nav'>
+      {!isLogged ? (
+        <nav className="header_nav">
           <Link to="/signup">
-            <h4 className='header_nav-link animate'>Créer un compte</h4>
+            <h4 className="header_nav-link animate">Créer un compte</h4>
           </Link>
           <Link to="/login">
-            <h4 className='header_nav-link animate'>Me connecter</h4>
+            <h4 className="header_nav-link animate">Me connecter</h4>
           </Link>
         </nav>
-        : <nav className='header_nav'>
+      ) : (
+        <nav className="header_nav">
           <Link to="/user">
-            <img className='header_nav_logo' src={avatar} alt="logo profile" />
+            <img className="header_nav_logo" src={avatar} alt="logo profile" />
+          </Link>
+          <Link to="/dashboard">
+            <h4 className="header_nav-link animate dc">Mes plannings</h4>
           </Link>
           <Link to="/">
-            <h4 onClick={handleLogout} className='header_nav-link animate dc'>Me déconnecter</h4>
+            <h4 onClick={handleLogout} className="header_nav-link animate dc">
+              Me déconnecter
+            </h4>
           </Link>
-        </nav>}
+        </nav>
+      )}
     </header>
   );
 }
