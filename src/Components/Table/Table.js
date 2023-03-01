8efba82dedf0a9900e0b0621b56adae262aab7ce
FabriceMilet <milet.fab@gmail.com>
2023-03-01 14:56:32 +0100

    ajout du bouton d'ajout de tache

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -2,14 +2,18 @@ import "./Table.scss";
 import Week from "./Week/Week";
 import WeekScroll from "./WeekScroll/WeekScroll";
 import { useSelector } from "react-redux";
+import AddTask from "./AddTask/AddTask";
 
 function Table() {
   // pour test
   const userConnected = useSelector((state) => state.user.userConnected);
-  console.log('from dashboard :', userConnected);
+  console.log("from dashboard :", userConnected);
   return (
     <div className="Table">
-      <WeekScroll />
+      <div className="Table-header">
+        <WeekScroll />
+        <AddTask />
+      </div>
       <Week />
     </div>
   );
