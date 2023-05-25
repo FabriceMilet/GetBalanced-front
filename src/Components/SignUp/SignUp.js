b9b8ee4a8f61f34d4c6924b634390e99c610bb10
FabriceMilet <milet.fab@gmail.com>
2023-05-25 14:54:32 +0200

    gestion du tuto pour mobiles

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -4,11 +4,18 @@ import { createUser } from "../../feature/user.slice";
 import { Link, Navigate } from 'react-router-dom';
 import { setFormData } from '../../feature/user.slice';
 import tutoGetBalanced from '../../img/tutoGetBalanced.jpg';
-import tutoGetBalanced1 from '../../img/tutoGetBalanced1.png';
+import tutoGetBalanced1 from '../../img/tutoGetBalanced1.jpg';
 import tutoGetBalanced2 from '../../img/tutoGetBalanced2.png';
 import tutoGetBalanced3 from '../../img/tutoGetBalanced3.png';
 import tutoGetBalanced4 from '../../img/tutoGetBalanced4.png';
 import tutoGetBalanced5 from '../../img/tutoGetBalanced5.png';
+import tutoGetBalancedMobile from '../../img/tutoGetBalancedMobile.jpg';
+import tutoGetBalancedMobile1 from '../../img/tutoGetBalancedMobile1.jpg';
+import tutoGetBalancedMobile2 from '../../img/tutoGetBalancedMobile2.jpg';
+import tutoGetBalancedMobile3 from '../../img/tutoGetBalancedMobile3.jpg';
+import tutoGetBalancedMobile4 from '../../img/tutoGetBalancedMobile4.jpg';
+import tutoGetBalancedMobile5 from '../../img/tutoGetBalancedMobile5.jpg';
+import tutoGetBalancedMobile6 from '../../img/tutoGetBalancedMobile6.jpg';
 import { useState } from "react";
 
 
@@ -23,6 +30,15 @@ export default function SignUp() {
     tutoGetBalanced4,
     tutoGetBalanced5
   ];
+  const tutorialImagesMobile = [
+    tutoGetBalancedMobile,
+    tutoGetBalancedMobile1,
+    tutoGetBalancedMobile2,
+    tutoGetBalancedMobile3,
+    tutoGetBalancedMobile4,
+    tutoGetBalancedMobile5,
+    tutoGetBalancedMobile6,
+  ]
   // j'ajoute un état  état pour gérer l'affichage du tutoriel et la position actuelle de l'image dans le tableau
   const [showTutorial, setShowTutorial] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -39,7 +55,7 @@ export default function SignUp() {
       });
     }
   };
-  
+
   // const prevImage = () => {
   //   if (currentImageIndex > 0) {
   //     setCurrentImageIndex(currentImageIndex - 1);
@@ -71,70 +87,75 @@ export default function SignUp() {
   return (
     <div>
       {showTutorial ? (
-         <div className="SignUp-tuto">
-         <img src={tutorialImages[currentImageIndex]} alt="Tutorial Step" />
-         {/* <button onClick={prevImage}>Précédent</button> */}
-           <button className="SignUp-next" onClick={nextImage}>Suivant</button>
-           </div>
-         ) : (
-    <form className="SignUp" onSubmit={handleSubmit}>
-      {!isLogged && (
-        <div className="SignUp-container">
-          <h1 className="SignUp-title"> Créez votre compte </h1>
-          <label htmlFor="prénom" className="SignUp-input">
-            <input
-              type="text"
-              name="firstname"
-              placeholder="Prénom"
-              value={formData.firstname}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="nom" className="SignUp-input">
-            <input
-              type="text"
-              name="lastname"
-              placeholder="Nom"
-              value={formData.lastname}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="email" className="SignUp-input">
-            <input
-              type="email"
-              name="email"
-              placeholder="Adresse Email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="Saisissez un mot de passe" className="SignUp-input">
-            <input
-              type="password"
-              name="password"
-              placeholder="Saisissez un mot de passe"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="Confirmez le mot de passe" className="SignUp-input">
-            <input
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirmez le mot de passe"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-            />
-          </label>
-          <button type="submit" className="SignUp-button">S'inscrire</button>
-          <Link onClick={handleClear} to="/login">
-            <p className="SignUp-link">Vous avez déjà un compte ?</p>
-          </Link>
+        <div className="SignUp-tuto">
+          <div className="SignUp-tutorialImages">
+            <img src={tutorialImages[currentImageIndex]} alt="Tutorial de présentation du site" />
+          </div>
+          <div className="SignUp-tutorialImagesMobile">
+            <img src={tutorialImagesMobile[currentImageIndex]} alt="Tutorial de présentation du site" />
+          </div>
+          {/* <button onClick={prevImage}>Précédent</button> */}
+          <button className="SignUp-next" onClick={nextImage}>Suivant</button>
         </div>
-      )}
-      {/* on veut renvoyer vers la page de profil si l'utilisateur est logué*/}
-      {isLogged && (<Navigate to="/user" replace />)}
-    </form>)}
+      ) : (
+        <form className="SignUp" onSubmit={handleSubmit}>
+          {!isLogged && (
+            <div className="SignUp-container">
+              <h1 className="SignUp-title"> Créez votre compte </h1>
+              <label htmlFor="prénom" className="SignUp-input">
+                <input
+                  type="text"
+                  name="firstname"
+                  placeholder="Prénom"
+                  value={formData.firstname}
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor="nom" className="SignUp-input">
+                <input
+                  type="text"
+                  name="lastname"
+                  placeholder="Nom"
+                  value={formData.lastname}
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor="email" className="SignUp-input">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Adresse Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor="Saisissez un mot de passe" className="SignUp-input">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Saisissez un mot de passe"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </label>
+              <label htmlFor="Confirmez le mot de passe" className="SignUp-input">
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirmez le mot de passe"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                />
+              </label>
+              <button type="submit" className="SignUp-button">S'inscrire</button>
+              <Link onClick={handleClear} to="/login">
+                <p className="SignUp-link">Vous avez déjà un compte ?</p>
+              </Link>
+            </div>
+          )}
+          {/* on veut renvoyer vers la page de profil si l'utilisateur est logué*/}
+          {isLogged && (<Navigate to="/user" replace />)}
+        </form>)}
     </div>
   );
 };
